052af0b4077b1074ccb3bf7af4636b4743fae87a
mbmuldoon <mbmuldoon07@gmail.com>
2023-02-08 14:58:57 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreenvilleRevenueGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int contestants_this_year = 0;
+            Int32.TryParse(textBox2.Text, out contestants_this_year);
+            label4.Text = (contestants_this_year*25).ToString();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Chp3GreenvilleRevenueGUI
+Chapter 3 Greenville Revenue Assignment Starter Project
+
+In Chapter 2, you created a program for the Greenville Idol competition that prompts a user for the number of contestants entered in last year’s 
+competition and in this year’s competition. The program displays the revenue expected for this year’s competition if each contestant pays a $25 entrance fee. The 
+application also displays a statement that indicates whether this year’s competition has more contestants than last year’s. 
+
+Now, create an interactive GUI program named GreenvilleRevenueGUI that performs all the same tasks.
